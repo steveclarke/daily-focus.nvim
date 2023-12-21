f4c42bac127b43ca3819a99732f203afd9b25acb
Steve Clarke <steve@sevenview.ca>
2023-12-20 23:41:56 -0330

    first code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Daily Focus
+
+A Neovim plugin to show a daily Vim tip to focus on for the day.
---
+++ lua/daily-focus/init.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.setup = function(opts)
+  print("Running Daily Focus setup...", vim.inspect(opts))
+end
+
+return M
