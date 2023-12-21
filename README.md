a34093c60333083608b8f5fb2f8bda75d3fceb60
Steve Clarke <steve@sevenview.ca>
2023-12-21 08:56:43 -0330

    create a command DailyFocusTip

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Daily Focus
 
-A Neovim plugin to show a daily Vim tip to focus on for the day.
+A Neovim plugin to return a daily Vim tip to focus on for the day. This can be used
+for things like adding to a Lualine section.
+
+> NOTE: This far from a general working plugin. There's lots of hard-coding to
+> my particular setup. I'm using it to learn how to make a Neovim plugin. I'll
+> remove this banner when it's ready for general use.
--- lua/daily-focus/init.lua
+++ lua/daily-focus/init.lua
@@ -4,41 +4,45 @@ local M = {}
 local tip_file_path = os.getenv("HOME") .. "/dotfiles/nvim/.config/nvim/data/tips.txt"
 
 local count_lines = function(filename)
-  local count = 0
-  for _ in io.lines(filename) do
-    count = count + 1
-  end
-  return count
+	local count = 0
+	for _ in io.lines(filename) do
+		count = count + 1
+	end
+	return count
 end
 
 M.tip = function()
-  local tip_file = io.open(tip_file_path, "r")
+	local tip_file = io.open(tip_file_path, "r")
 
-  if not tip_file then
-    print("Error opening tip file " .. tip_file_path)
-    return nil
-  end
+	if not tip_file then
+		print("Error opening tip file " .. tip_file_path)
+		return nil
+	end
 
-  if count_lines == 0 then
-    print("Tip file is empty")
-    tip_file:close()
-    return nil
-  end
+	if count_lines == 0 then
+		print("Tip file is empty")
+		tip_file:close()
+		return nil
+	end
 
-  local random_line_number = math.random(1, count_lines(tip_file_path))
-  local current_line = 0
+	local random_line_number = math.random(1, count_lines(tip_file_path))
+	local current_line = 0
 
-  for line in tip_file:lines() do
-    current_line = current_line + 1
-    if current_line == random_line_number then
-      tip_file:close()
-      return " Today's focus:  " .. line
-    end
-  end
+	for line in tip_file:lines() do
+		current_line = current_line + 1
+		if current_line == random_line_number then
+			tip_file:close()
+			return " Today's focus:  " .. line
+		end
+	end
 end
 
 M.setup = function(opts)
-  print("Options:", vim.inspect(opts))
+	print("Options:", vim.inspect(opts))
 end
+
+vim.api.nvim_create_user_command("DailyFocusTip", function()
+	print(M.tip())
+end, { bang = true, desc = "Print a random tip from the tip file" })
 
 return M
