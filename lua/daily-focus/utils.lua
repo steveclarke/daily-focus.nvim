d0eab4eed38d4a0dbfcb0dfb0582763c453a7fd0
Steve Clarke <steve@sevenview.ca>
2023-12-23 11:26:30 -0330

    wip

--- lua/daily-focus/utils.lua
+++ lua/daily-focus/utils.lua
@@ -1,11 +1,23 @@
 local M = {}
 
+local default_data_file = vim.fn.json_encode({
+	current_line = 1,
+	current_date = "",
+	current_tip = "",
+})
+
 local make_data_file = function(config)
-	print("Making data file " .. config.data_file .. "...")
 	local data_file = config.data_file
+
 	if vim.fn.filereadable(data_file) == 0 then
 		local file = io.open(data_file, "w")
-		file:write("{}")
+
+		if file == nil then
+			print("Could not open file " .. data_file)
+			return
+		end
+
+		file:write(default_data_file)
 		file:close()
 	end
 end
